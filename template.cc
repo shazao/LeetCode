dc7bf1fb15aceacf04b3046c7c9e485eac97ea20
Qionghua Yang <yangqionghuasmailbox@126.com>
2015-09-16 08:25:07 +0800

    Modification.
    Added Class Solution.

--- template.cc
+++ template.cc
@@ -8,6 +8,23 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include "..\Profiler.h"
+
+class Solution {
+  public:
+    virtual  = 0; // Pure virtual, or Solutionx may not have data members.
+};
+
+class Solution0 : public Solution {
+  public:
+    
+};
+
+class Solution1 : public Solution {
+  public:
+    
+};
+
 
 int main(int argc, char * argv[]) {
 
@@ -18,9 +35,6 @@ int main(int argc, char * argv[]) {
 
   int n = atoi(argv[1]);
 
-  return 0;
-}
-
   std::vector<int> iv;
   for (int i=1; i<argc; ++i)
     iv.push_back(std::atoi(argv[i]));
@@ -28,3 +42,14 @@ int main(int argc, char * argv[]) {
   for (size_t i=0; i<iv.size(); ++i)
     std::cout << iv[i] << " ";
   std::cout << std::endl;
+
+  std::vector<Solution*> solutions;
+  Solution0 s0; solutions.push_back(&s0);
+  Solution1 s1; solutions.push_back(&s1);
+  for (size_t si=0; si<solutions.size(); ++si) {
+    std::cout << "\n\t\t=== Solution " << si << " ===\n" << std::endl;
+    Profiler perf;
+  }
+
+  return 0;
+}
