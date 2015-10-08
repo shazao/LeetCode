22ebc71d28c758de4d8d934fd2f6da08f685a9f0
Qionghua Yang <yangqionghuasmailbox@126.com>
2015-10-08 11:25:26 +0800

    Improved.
    Two methods added.

--- 021_merge_two_sorted_lists/merge_two_sorted_lists.cc
+++ 021_merge_two_sorted_lists/merge_two_sorted_lists.cc
@@ -6,6 +6,8 @@
 // Info.: 30 minutes, one-time pass.
 
 #include <iostream>
+#include <vector>
+#include "..\Profiler.h"
 
 struct ListNode {
     int val;
@@ -22,46 +24,107 @@ void printList(ListNode * head) {
   std::cout << node->val << std::endl;
 }
 
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-  if (l1 == NULL)
-    return l2;
-  if (l2 == NULL)
-    return l1;
+class Solution {
+  public:
+    virtual ListNode * mergeTwoLists(ListNode* l1, ListNode* l2) = 0; // Pure virtual, or Solutionx may not have data members.
+};
 
-  ListNode * node1 = l1;
-  ListNode * node2 = l2;
+// My solution.
+class Solution0 : public Solution {
+  public:
+    ListNode * mergeTwoLists(ListNode* l1, ListNode* l2) {
+      if (l1 == NULL)
+        return l2;
+      if (l2 == NULL)
+        return l1;
 
-  ListNode * head = l1;
-  if (l1->val > l2->val) {
-    head = l2;
-    node2 = l2->next;
-  } else 
-    node1 = l1->next;
+      ListNode * node1 = l1;
+      ListNode * node2 = l2;
 
-  ListNode * node = head;
-  while (node1 && node2) {
-    if (node1->val < node2->val) {
-      node->next = node1;
-      node1 = node1->next;
-    } else {
-      node->next = node2;
-      node2 = node2->next;
+      ListNode * head = l1;
+      if (l1->val > l2->val) {
+        head = l2;
+        node2 = l2->next;
+      } else 
+        node1 = l1->next;
+
+      ListNode * node = head;
+      while (node1 && node2) {
+        if (node1->val < node2->val) {
+          node->next = node1;
+          node1 = node1->next;
+        } else {
+          node->next = node2;
+          node2 = node2->next;
+        }
+        node = node->next;
+      }
+      if (node1 == NULL)
+        node->next = node2;
+      else
+        node->next = node1;
+
+      return head;
     }
-    node = node->next;
-  }
-  if (node1 == NULL)
-    node->next = node2;
-  else
-    node->next = node1;
+};
 
-  return head;
-}
+// A cleaner one from LeetCode Discuss.
+class Solution1 : public Solution {
+  public:
+    ListNode * mergeTwoLists(ListNode* l1, ListNode* l2) {
+      if (l1 == NULL)
+        return l2;
+      if (l2 == NULL)
+        return l1;
 
-int main() {
+      ListNode * dummy = new ListNode(0);
+      ListNode * tail = dummy;
 
-  std::cout << "Original list: ";
+      while (l1 && l2) {
+        if (l1->val < l2->val) {
+          tail->next = l1;
+          l1 = l1->next;
+        } else {
+          tail->next = l2;
+          l2 = l2->next;
+        }
+        tail = tail->next;
+      }
 
-  ListNode * l1 = new ListNode(1);
+      tail->next = l1 ? l1 : l2;
+
+      ListNode * result = dummy->next;
+      delete dummy;
+      return result;
+    }
+};
+
+// Recursive method.
+class Solution2 : public Solution {
+  public:
+    ListNode * mergeTwoLists(ListNode* l1, ListNode* l2) {
+      if (l1 == NULL)
+        return l2;
+      if (l2 == NULL)
+        return l1;
+
+      if (l1->val < l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+      } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+      }
+    }
+};
+
+static ListNode * l1 = NULL;
+static ListNode * l2 = NULL;
+
+void createLists() {
+  std::cout << "Original list: " << std::endl;
+
+  l1 = new ListNode(1);
   ListNode * node = l1;
   for (int i=0; i<=5; ++i) {
     while (node->next)
@@ -70,7 +133,7 @@ int main() {
   }
   printList(l1);
 
-  ListNode * l2 = new ListNode(1);
+  l2 = new ListNode(1);
   node = l2;
   for (int i=1; i<=7; ++i) {
     while (node->next)
@@ -78,9 +141,30 @@ int main() {
     node->next = new ListNode(2*i);
   }
   printList(l2);
+}
 
-  std::cout << "Merged list: ";
-  printList(mergeTwoLists(l1, l2));
+void deleteList(ListNode * l) {
+  while (l) {
+    ListNode * next = l->next;
+    delete l;
+    l = next;
+  }
+}
 
+int main() {
+
+  std::vector<Solution*> solutions;
+  Solution0 s0; solutions.push_back(&s0);
+  Solution1 s1; solutions.push_back(&s1);
+  Solution2 s2; solutions.push_back(&s2);
+  for (size_t si=0; si<solutions.size(); ++si) {
+    std::cout << "\n\t\t=== Solution " << si << " ===\n" << std::endl;
+    createLists();
+    std::cout << "Merged list: ";
+    Profiler perf;
+    ListNode * sl = solutions[si]->mergeTwoLists(l1, l2);
+    printList(sl);
+    deleteList(sl);
+  }
   return 0;
 }
