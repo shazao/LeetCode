ba1c3ef9c9f685faf82cce6d8064ec1c9a0ee6bf
Qionghua Yang <yangqionghuasmailbox@126.com>
2015-11-02 20:52:39 +0800

    Added a solution.

--- 077_combinations/combinations.cc
+++ 077_combinations/combinations.cc
@@ -27,34 +27,55 @@ class Solution {
     virtual std::vector<std::vector<int> > combine(int n, int k) = 0; // Pure virtual, or Solutionx may not have data members.
 };
 
-/* From LeetCode Discuss: First, choose a number and push_back; 
- * Second, choose a larger number; Until k number chose. */
 class Solution0 : public Solution {
   public:
     std::vector<std::vector<int> > combine(int n, int k) {
-      if (n<k || k<=0) return combinations;
-      combination.resize(k);
+      if (n<k || k<=0) return combinations_;
       combine(1, n, k);
-      return combinations;
+      return combinations_;
     }
     
   private:
-    std::vector<std::vector<int> > combinations;
-    std::vector<int> combination;
+    std::vector<std::vector<int> > combinations_;
+    std::vector<int> combination_;
 
     void combine(int start, int n, int k) {
-      while (start <= n) {
-        combination[combination.size() - k] = start++;
-        if (k > 1)
-          combine(start, n, k-1);
-        else
-          combinations.push_back(combination);
+      if (combination_.size() == k) {
+        combinations_.push_back(combination_);
+        return ;
+      }
+      for (size_t i=start; i<=n; ++i) {
+        combination_.push_back(i);
+        combine(i+1, n, k);
+        combination_.pop_back();
       }
     }
 };
 
+/* From LeetCode Discuss: First, choose a number and push_back; 
+ * Second, choose a larger number; Until k number chose. */
 class Solution1 : public Solution {
   public:
+    std::vector<std::vector<int> > combine(int n, int k) {
+      if (n<k || k<=0) return combinations_;
+      combination_.resize(k);
+      combine(1, n, k);
+      return combinations_;
+    }
+    
+  private:
+    std::vector<std::vector<int> > combinations_;
+    std::vector<int> combination_;
+
+    void combine(int start, int n, int k) {
+      while (start <= n) {
+        combination_[combination_.size() - k] = start++;
+        if (k > 1)
+          combine(start, n, k-1);
+        else
+          combinations_.push_back(combination_);
+      }
+    }
 };
 
 int main(int argc, char * argv[]) {
@@ -70,7 +91,7 @@ int main(int argc, char * argv[]) {
 
   std::vector<Solution*> solutions;
   Solution0 s0; solutions.push_back(&s0);
-  //Solution1 s1; solutions.push_back(&s1);
+  Solution1 s1; solutions.push_back(&s1);
   for (size_t si=0; si<solutions.size(); ++si) {
     std::cout << "\n\t\t=== Solution " << si << " ===\n" << std::endl;
     Profiler perf;
