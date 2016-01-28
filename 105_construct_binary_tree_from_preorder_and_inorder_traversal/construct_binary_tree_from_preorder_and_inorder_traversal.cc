5bebf137012492a06cb65b2c194244ccc7e4cfe9
Qionghua Yang <yangqionghuasmailbox@126.com>
2016-01-28 21:21:55 +0800

    Medium 105.
    Iterative solution.

--- 105_construct_binary_tree_from_preorder_and_inorder_traversal/construct_binary_tree_from_preorder_and_inorder_traversal.cc
+++ 105_construct_binary_tree_from_preorder_and_inorder_traversal/construct_binary_tree_from_preorder_and_inorder_traversal.cc
@@ -6,6 +6,7 @@ You may assume that duplicates do not exist in the tree.
 */
 
 // Star: 8.8.
+// TODO: Figure out the iterative solution.
 
 #include <iostream>
 #include <vector>
@@ -45,9 +46,36 @@ class Solution0 : public Solution {
     }
 };
 
+// Iterative solution from LeetCode Discuss; Beats 92.35%.
 class Solution1 : public Solution {
   public:
-    
+    TreeNode *buildTree(vector<int> &pre, vector<int> &in) {
+      int i = 0, j = 0;
+      TreeNode * root = new TreeNode(0x80000000);
+      TreeNode * pp = NULL, * ptr = root;
+      std::stack<TreeNode*> sn;
+      sn.push(root);
+      while (j < in.size()) {
+          if (sn.top()->val == in[j]) {
+            pp=sn.top();
+            sn.pop();
+            j++;
+          } else if (pp) {
+            ptr=new TreeNode(pre[i]);
+            pp->right=ptr;pp=NULL;
+            sn.push(ptr);
+            i++;
+          } else {
+            ptr=new TreeNode(pre[i]);
+            sn.top()->left=ptr;
+            sn.push(ptr);
+            i++;
+          }
+      }
+      ptr=root->left;
+      delete root;
+      return ptr;
+    }
 };
 
 
