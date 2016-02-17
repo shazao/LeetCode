ed9b076f9a9f6121013f9c6c589a1fda9e4fa775
Qionghua Yang <yangqionghuasmailbox@126.com>
2016-02-17 22:08:44 +0800

    Medium 116.
    LeetCode 116, Category Medium.

--- 116_populating_next_right_pointers_in_each_node/populating_next_right_pointers_in_each_node.cc
+++ 116_populating_next_right_pointers_in_each_node/populating_next_right_pointers_in_each_node.cc
@@ -29,7 +29,8 @@ After calling your function, the tree should look like:
     4->5->6->7 -> NULL
 */
 
-// Star: 
+// Star: 8.8.
+// Info.: Not bad.
 
 #include <iostream>
 #include <vector>
@@ -41,6 +42,7 @@ struct TreeLinkNode {
  int val;
  TreeLinkNode *left, *right, *next;
  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ TreeLinkNode(int x, TreeLinkNode * left, TreeLinkNode * right) : val(x), left(left), right(right), next(NULL) {}
 };
 
 class Solution {
@@ -48,15 +50,61 @@ class Solution {
     virtual void connect(TreeLinkNode * root) = 0; // Pure virtual, or Solutionx may not have data members.
 };
 
+// Iterative method.
 class Solution0 : public Solution {
   public:
+    void connect(TreeLinkNode * root) {
+      if (root) {
+        TreeLinkNode * lmn = root;  // Left most node.
+        TreeLinkNode * lln = NULL;  // Last layer node.
+        while (lmn->left) {
+          lln = lmn;
+          while (lln) {
+            lln->left->next = lln->right;
+            TreeLinkNode * next = lln->next;
+            if (next)
+              lln->right->next = next->left;
+            lln = next;
+          }
+          lmn = lmn->left;
+        }
+      }
+      return;
+    }
 };
 
+// Recursive method.
 class Solution1 : public Solution {
   public:
-    
+    void connect(TreeLinkNode * root) {
+      if (root) connect(root->left, root->right);
+    }
+  private:
+    void connect(TreeLinkNode * n0, TreeLinkNode * n1) {
+      if (n0 && n1) {
+        n0->next = n1;
+        connect(n0->left, n0->right);
+        connect(n0->right, n1->left);
+        connect(n1->left, n1->right);
+      }
+    }
 };
 
+void print(TreeLinkNode * root) {
+  if (root) {
+    TreeLinkNode * lmn = root;
+    while (lmn) {
+      TreeLinkNode * tln = lmn;
+      while (tln) {
+        std::cout << ' ' << tln->val;
+        tln = tln->next;
+      }
+      std::cout << std::endl;
+      lmn = lmn->left;
+    }
+  }
+  return ;
+}
 
 int main(int argc, char * argv[]) {
 
@@ -65,24 +113,23 @@ int main(int argc, char * argv[]) {
     return -1;
   }
 
-  // Get an array.
-  std::cout << "Please input the array: ";
-  std::vector<int> iv;
-  int i = 0;
-  while (std::cin >> i)
-    iv.push_back(i);
-  std::cout << "The array you input is: ";
-  for (auto itr=iv.begin(); itr!=iv.end(); ++itr)
-    std::cout << ' ' << *itr;
-  std::cout << std::endl;
+  TreeLinkNode * _3 = new TreeLinkNode(3);
+  TreeLinkNode * _4 = new TreeLinkNode(4);
+  TreeLinkNode * _5 = new TreeLinkNode(5);
+  TreeLinkNode * _6 = new TreeLinkNode(6);
+  TreeLinkNode * _1 = new TreeLinkNode(1, _3, _4);
+  TreeLinkNode * _2 = new TreeLinkNode(2, _5, _6);
+  TreeLinkNode * root = new TreeLinkNode(0, _1, _2);
 
   std::vector<Solution*> solutions;
-  Solution0 s0; solutions.push_back(&s0);
+  //Solution0 s0; solutions.push_back(&s0);
   Solution1 s1; solutions.push_back(&s1);
   for (size_t si=0; si<solutions.size(); ++si) {
     std::cout << "\n\t\t=== Solution " << si << " ===\n" << std::endl;
     Profiler perf;
-    solutions[si]->;
+    solutions[si]->connect(root);
+    std::cout << "The connected tree: " << std::endl;
+    print(root);
   }
 
   return 0;
